8cd15c25b20168f7add805e6c5b606f19dba2c02
Juan Edi <juan@noredink.com>
2021-10-12 00:17:51 -0300

    install ruby

--- shell.nix
+++ shell.nix
@@ -1,10 +1,11 @@
 let
   sources = import ./nix/sources.nix {};
   niv = import sources.niv {};
-  pkgs = import sources.nixpkgs {};
-in with pkgs;
-mkShell {
+  pkgs = import sources.nixpkgs-unstable {};
+in
+pkgs.mkShell {
   buildInputs = [
     niv.niv
+    pkgs.ruby
   ];
 }
